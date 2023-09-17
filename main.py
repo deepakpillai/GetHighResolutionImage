e0d31508e4e8ad3338198e277726785e1810b0de
Deepak Pillai <pillai.deepakb@gmail.com>
2023-09-17 11:43:13 +0530

    Deconvolution applied

--- main.py
+++ main.py
@@ -1,111 +1,150 @@
+import pandas as pd
 import torch
-import torchvision
-from PIL import Image
-import os
-from torchvision import transforms
-import random
-import matplotlib.pyplot as plt
 from torch import nn
-from tqdm.auto import tqdm
+import torchvision
+from torchvision import transforms
 from torch.utils.data.dataloader import DataLoader
+from PIL import Image
+from tqdm.auto import tqdm
+from torch.optim import lr_scheduler
+import os
+import cv2
+import matplotlib.pyplot as plt
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
-def showRandomImages():
-    random_int = random.randint(0, len(train_array))
-    low_image, high_image = train_array[random_int]
-    low_image = low_image.permute(1, 2, 0)
-    high_image = high_image.permute(1, 2, 0)
-    plt.figure(figsize=(5, 3))
-    plt.subplot(1, 2, 1)
-    plt.imshow(low_image)
-    plt.subplot(1, 2, 2)
-    plt.imshow(high_image)
-    plt.show()
 
-train_array = []
-low_res_path = './dataset/train/low_res/'
-high_res_path = './dataset/train/high_res/'
-files = os.listdir(low_res_path)
+pre_process = transforms.Compose([
+    transforms.Resize(size=(320, 320)),
+    transforms.ToTensor(),
+])
 
-preprocess_image = transforms.Compose([
-    transforms.Resize(size=(256, 256)),
-    transforms.RandomGrayscale(0.3),
-    transforms.RandomAdjustSharpness(0.3),
+resize_transforms = transforms.Compose([
+    transforms.Resize(size=(336, 336)),
     transforms.ToTensor()
 ])
 
-high_process_image = transforms.Compose([
-    transforms.Resize(size=(768, 768)),
-    transforms.ToTensor()
-])
+# https://www.kaggle.com/datasets/quadeer15sh/image-super-resolution-from-unsplash?resource=download
+dataset = []
 
-for index, pic in enumerate(files):
-    low_image_path = low_res_path + pic
-    high_image_path = high_res_path + pic
-    low_image = Image.open(low_image_path)
-    low_image = low_image.convert('RGB')
-    low_image = preprocess_image(low_image)
-    high_image = Image.open(high_image_path)
-    high_image = high_image.convert('RGB')
-    high_image = high_process_image(high_image)
-    data_tupple = (low_image, high_image) #X, y
-    train_array.append(data_tupple)
+legend = pd.read_csv('./image_data.csv')
+legent_df = pd.DataFrame(legend)
+# shuffle the DataFrame rows
+# legent_df = legent_df.sample(frac = 1)
+for index in legent_df.index:
+    low_image = legent_df['low_res'][index]
+    high_res = legent_df['high_res'][index]
+
+    low_path = "./dataset/low_res/"
+    high_path = "./dataset/high_res/"
+
+    low_res_image_path = low_path + low_image
+    high_res_image_path = high_path + high_res
 
 
-class HighResModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv_bock_1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.conv_bock_2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.conv_bock_3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.conv_bock_4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
-        self.conv_bock_5 = nn.Conv2d(in_channels=512, out_channels=27, kernel_size=3, stride=1, padding=1)
-        self.upscale = nn.PixelShuffle(3) #input image size is 256 hence upscalling to 1024
+    low_res_image = pre_process(Image.open(low_res_image_path))
+    high_res_image = resize_transforms(Image.open(high_res_image_path))
+    low_res_image = low_res_image.to(device)
+    high_res_image = high_res_image.to(device)
+    image_tupple = (low_res_image, high_res_image)
+    dataset.append(image_tupple)
+    # if index == 1:
+    #     break
 
+
+# low, high = dataset[8]
+# low = low.permute(1,2,0).cpu()
+# plt.subplot(1,2,1)
+# plt.imshow(low)
+# plt.subplot(1,2,2)
+# plt.imshow(high.permute(1,2,0).cpu())
+# plt.show()
+
+# low_res_path = './dataset_256/low_res/'
+# high_res_path = './dataset_256/high_res/'
+# files = os.listdir(low_res_path)
+
+# for index, pic in enumerate(files):
+#     low_image_path = low_res_path + pic
+#     high_image_path = high_res_path + pic
+#     low_image = Image.open(low_image_path)
+#     low_image = low_image.convert('RGB')
+#     low_image = pre_process(low_image)
+#     high_image = Image.open(high_image_path)
+#     high_image = high_image.convert('RGB')
+#     high_image = resize_transforms(high_image)
+#     data_tupple = (low_image, high_image) #X, y
+#     dataset.append(data_tupple)
+
+
+batch_size = 8
+batch_data = DataLoader(dataset, batch_size=batch_size)
+
+
+
+class HDModel(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.cnnblock = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=512, out_channels=512, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=32, out_channels=30, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=30, out_channels=29, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=29, out_channels=3, kernel_size=3, stride=1),
+            # nn.PixelShuffle(2)
+            
+        )
+    
     def forward(self, x):
-        x = self.conv_bock_1(x)
-        x = self.conv_bock_2(x)
-        x = self.conv_bock_3(x)
-        x = self.conv_bock_4(x)
-        x = self.conv_bock_5(x)
-        x = self.upscale(x)
-        return x
+        data = self.cnnblock(x)
+        return data
+    
+# testing model
+# sample_data = torch.randn(3, 256, 256)
+# model = HDModel()
+# model(sample_data)
 
-
-batch_data = DataLoader(train_array, batch_size=8)
-model = HighResModel().to(device)
-
-# Print the model architecture and check its parameters
-print(model)
-print("Number of parameters:", sum(p.numel() for p in model.parameters()))
-
-loss_fn = nn.L1Loss()
-optim_fn = torch.optim.Adam(params=model.parameters(), lr=0.001)
-
-for epoch in tqdm(torch.arange(0, 10)):
-    for batch, (input, label) in enumerate(batch_data):
-        input = input.to(device)
+total_epoch = 20
+model = HDModel().to(device)
+loss_fn = nn.MSELoss()
+optim_fn = torch.optim.AdamW(model.parameters(), lr=0.001)
+# scheduler = lr_scheduler.LinearLR(optimizer=optim_fn, start_factor=1.0, end_factor=0.5, total_iters=total_epoch)
+model.train()
+for epoch in tqdm(range(0, total_epoch)):
+    batch_loss = 0
+    for batch, (feature, label) in enumerate(dataset):
+        
+        feature = feature.to(device)
         label = label.to(device)
-        model.train()
-        pred = model(input)
+        pred = model(feature)
         loss = loss_fn(pred, label)
+        batch_loss = batch_loss + loss
         optim_fn.zero_grad()
         loss.backward()
         optim_fn.step()
 
         if batch % 8 == 0:
-            print(f"Loss {loss}")
+            print(f"batch_loss: {batch_loss/batch_size}")
 
-torch.save(model, 'model.pth')
-torch.save(model.state_dict(), './models.pth')
-model.eval()
-random_int = random.randint(0, len(train_array))
-low_image, high_image = train_array[random_int]
-output = low_image.permute(1,2,0)
-plt.imshow(output)
-plt.show()
-pred = model(low_image)
-output_new = low_image.permute(1,2,0)
-plt.imshow(output_new)
-plt.show()
+    # before_lr = optim_fn.param_groups[0]['lr']
+    # scheduler.step()
+    # after_lr = optim_fn.param_groups[0]['lr']
+    # print(f"Epoch: {epoch}, before_lr: {before_lr}, after_lr: {after_lr}")
+
+torch.save(model, "./model.pth")
+torch.save(model.state_dict(), "./model_state_dict.pth")
